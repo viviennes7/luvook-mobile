66e57dd17af2c3b5618593ea7464215b5199b4c1
yl_Minsoo <viviennes7@naver.com>
2017-07-21 16:39:07 +0900

    update item

--- src/pages/common/item/item.ts
+++ src/pages/common/item/item.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'modal-item',
   templateUrl: 'item.html'
 })
 export class ItemModal {
-
-  constructor() {
-
+  constructor(public viewCtrl: ViewController) {}
+  dismiss(){
+    this.viewCtrl.dismiss();
   }
-
 }
