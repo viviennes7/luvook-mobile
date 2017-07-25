ae9219ae2f28d2037561a7f3876ef187d9ddcd5d
yl_Minsoo <viviennes7@naver.com>
2017-07-25 16:31:25 +0900

    update post-contents

--- src/pages/post/contents/contents.ts
+++ src/pages/post/contents/contents.ts
@@ -7,6 +7,10 @@ import { NavController } from 'ionic-angular';
 })
 export class PostContentsPage {
 
+  stars: string[] = [
+    'star-outline', 'star-outline', 'star-outline', 'star-outline', 'star-outline'
+  ];
+
   constructor(public navCtrl: NavController,
               private elementRef:ElementRef,
               private renderer:Renderer ) {}
@@ -17,5 +21,19 @@ export class PostContentsPage {
         let textarea = this.elementRef.nativeElement.querySelector('.contents-textarea');
         textarea.focus();
       });
+  }
+
+  onClickStar(num){
+    this.initStar();
+    
+    for(let i=0; i<num; i++){
+      this.stars[i] = 'star';
+    }
+  }
+
+  initStar(){
+    for(let i = 0; i < 5; i++){
+      this.stars[i] = 'star-outline';
+    }
   }
 }
