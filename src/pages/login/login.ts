c87d6350fea7337077ea657d65c3e828c1af88af
김민수 <viviennes7@naver.com>
2017-08-20 16:19:35 +0900

    add login()

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,23 +3,50 @@ import { NavController, ViewController, ModalController, App  } from 'ionic-angu
 
 import{ TabsPage } from '../tabs/tabs';
 import{ JoinPage } from './join/join';
+import {Http, RequestOptions, Headers} from "@angular/http";
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+
+    email: string;
+    password: string;
+
   constructor(public navCtrl: NavController,
               private viewCtrl: ViewController,
               public modalCtrl: ModalController,
-              private appCtrl: App) {}
+              private appCtrl: App,
+              private http: Http
+  ) {}
 
   ionViewWillEnter() {
     this.viewCtrl.showBackButton(false);
   }
 
-  locateMainPage(){
-    this.appCtrl.getRootNav().setRoot(TabsPage);
+  login(){
+    if(!this.email){
+      alert("이메일을 입력해주세요.");
+      return;
+    }else if(!this.password){
+      alert("비밀번호를 입력해주세요.");
+      return;
+    }
+
+    let params = JSON.stringify({email : this.email, password : this.password});
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    this.http
+        .post("http://localhost:8080/member/signin", params, {headers:headers})
+        .subscribe(data =>{
+          let result = data.json();
+
+          if(result.statusCode == 200){
+            this.appCtrl.getRootNav().setRoot(TabsPage);
+          }else{
+            alert(result.message);
+          }
+        });
   }
 
   openJoinModal() {
