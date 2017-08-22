750c5647d0560d6f884bfb663e01e6abbb5510b0
yl_Minsoo <viviennes7@naver.com>
2017-08-23 05:26:47 +0900

    modify first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,34 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
+import { HttpService } from '../services/http.service';
 import { TabsPage } from '../pages/tabs/tabs';
 import { WelcomePage } from '../pages/welcome/welcome'
 import {LoginPage} from "../pages/login/login";
+import {TimelinePage} from "../pages/timeline/timeline";
+import {StorageService} from "../services/storage.service";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = null;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.storage.get("jwt").then(jwt => {
+        if(jwt == null){
+          this.rootPage = LoginPage;
+        }else{
+          HttpService.AUTH = jwt;
+          this.rootPage = TabsPage;
+        }
+      });
     });
   }
 }
