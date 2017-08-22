4afd40659297437d5f7cd168be552fe68610cf07
yl_Minsoo <viviennes7@naver.com>
2017-08-23 05:27:47 +0900

    update storage

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -2,13 +2,18 @@ import {Injectable} from "@angular/core";
 import { Storage } from '@ionic/storage';
 import { Observable } from 'rxjs/Rx';
 
+import { Member } from '../datas/member';
+
 @Injectable()
 export class StorageService{
+
   constructor(private storage: Storage){}
 
   setJwt(jwt:string){
     this.storage.set("jwt", jwt);
   }
+
+  //this.storageService.get("jwt").subscribe(data => alert("RX ::: " + data));
 
   get(key: string){
     return Observable.fromPromise(
@@ -16,6 +21,6 @@ export class StorageService{
         return val;
       })
     );
-
   }
+
 }
