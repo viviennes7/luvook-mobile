4dfb0825591d84f5f5e8f8fc687a92a94fb8799a
yl_Minsoo <viviennes7@naver.com>
2017-08-26 02:58:26 +0900

    add ItemComponent function

--- src/component/item/item.component.ts
+++ src/component/item/item.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController, ViewController } from 'ionic-angular';
+import { NavController, ViewController, NavParams, LoadingController, Loading } from 'ionic-angular';
 
 import { PhotoViewerUtil } from '../../utils/photo-viewer';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../datas/book';
 
 @Component({
   selector: 'modal-item',
   templateUrl: 'item.component.html'
 })
 export class ItemComponent {
-  constructor(public viewCtrl: ViewController,
-              private photoViewerUtil: PhotoViewerUtil) {}
+
+  private isbn13:string;
+  private book: Book = new Book();
+  private loading: Loading;
+
+  constructor(private viewCtrl: ViewController,
+              private photoViewerUtil: PhotoViewerUtil,
+              private params: NavParams,
+              private loadingCtrl: LoadingController,
+              private bookService: BookService) {
+
+    this.isbn13 = params.get("isbn13");
+    this.getBook();
+    this.loading = this.loadingCtrl.create();
+    this.loading.present();
+
+  }
+
+  private getBook(){
+    this.bookService.get(this.isbn13).subscribe(res =>{
+      let result = res.json();
+      this.book = result;
+    });
+  }
+
+
   dismiss(){
     this.viewCtrl.dismiss();
   }
