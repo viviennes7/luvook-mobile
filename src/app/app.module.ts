b1984fe640c97096b30c573c0e7a0ee370a66da9
김민수 <viviennes7@naver.com>
2017-09-10 19:55:05 +0900

    remove File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,32 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { PostPage } from '../pages/post/post';
-import { MyPage } from '../pages/my/my';
-import { TimelinePage } from '../pages/timeline/timeline';
-import { SearchPage } from '../pages/search/search';
-import { TabsPage } from '../pages/tabs/tabs';
-import { WelcomePage } from '../pages/welcome/welcome'
-import { LoginPage } from '../pages/login/login';
-import { JoinPage } from '../pages/login/join/join';
-import { SettingPage } from '../pages/my/setting/setting';
-import { DetailViewComponent } from "../component/detailview/detailview.component";
-import { PostContentsPage } from "../pages/post/contents/contents";
-import { CommentPage } from "../pages/comment/comment";
-import { ItemComponent } from "../component/item/item.component";
-import { BoardComponent } from "../component/board/board.component";
-import { CommentComponent } from "../component/comment/comment.component";
-import { CommentWriteComponent } from "../component/comment/comment-write.component";
-import { ImagePickerPage } from "../pages/image-picker/image-picker";
-import { PhotoViewerUtil } from "../utils/photo-viewer";
-
-import { File } from '@ionic-native/file';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Camera } from '@ionic-native/camera';
-import { PhotoViewer } from '@ionic-native/photo-viewer';
-import { ImagePicker } from '@ionic-native/image-picker';
-import {Http, HttpModule} from "@angular/http";
+import {PostPage} from '../pages/post/post';
+import {MyPage} from '../pages/my/my';
+import {TimelinePage} from '../pages/timeline/timeline';
+import {SearchPage} from '../pages/search/search';
+import {TabsPage} from '../pages/tabs/tabs';
+import {WelcomePage} from '../pages/welcome/welcome'
+import {LoginPage} from '../pages/login/login';
+import {JoinPage} from '../pages/login/join/join';
+import {SettingPage} from '../pages/my/setting/setting';
+import {DetailViewComponent} from "../component/detailview/detailview.component";
+import {PostContentsPage} from "../pages/post/contents/contents";
+import {CommentPage} from "../pages/comment/comment";
+import {ItemComponent} from "../component/item/item.component";
+import {BoardComponent} from "../component/board/board.component";
+import {CommentComponent} from "../component/comment/comment.component";
+import {CommentWriteComponent} from "../component/comment/comment-write.component";
+import {ImagePickerPage} from "../pages/image-picker/image-picker";
+import {PhotoViewerUtil} from "../utils/photo-viewer";
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Camera} from '@ionic-native/camera';
+import {PhotoViewer} from '@ionic-native/photo-viewer';
+import {ImagePicker} from '@ionic-native/image-picker';
+import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 import {IonicStorageModule} from "@ionic/storage";
 import {JwtService} from "../services/jwt.service";
@@ -96,7 +94,6 @@ import {AutosizeModule} from "angular2-autosize";
     StatusBar,
     SplashScreen,
     Camera,
-    File,
     ImagePicker,
     PhotoViewer,
     PhotoViewerUtil,
