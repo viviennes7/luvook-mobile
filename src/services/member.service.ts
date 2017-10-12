e320b7ef88db4148186455fbf216f7e48fd708b5
yl_Minsoo <viviennes7@naver.com>
2017-10-12 11:55:58 +0900

    remove timeline data at logout()

--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -3,6 +3,7 @@ import { App } from 'ionic-angular';
 
 import {Member} from '../datas/member';
 import {JwtService} from './jwt.service';
+import {TimelineService} from './timeline.service';
 import {LoginPage} from '../pages/login/login';
 
 @Injectable()
@@ -10,10 +11,12 @@ export class MemberService{
   myInfo: Member;
 
   constructor(private jwtService: JwtService,
+              private timelineService: TimelineService,
               private appCtrl: App){}
 
 
   logout(){
+    this.timelineService.bookBoards = [];
     this.jwtService.set(null);
     this.appCtrl.getRootNav().setRoot(LoginPage);
   }
